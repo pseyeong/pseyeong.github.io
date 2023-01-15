23ac70afc218f4875c8fbd6d00dee09d08b2039a
seyeong <yeongpsy@gmail.com>
2023-01-15 10:46:35 +0900

    230115_dist 복붙

---
+++ js/about.c42ef939.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkwebpackprj01"]=self["webpackChunkwebpackprj01"]||[]).push([[443],{7381:function(n,a,e){e.r(a),e.d(a,{default:function(){return b}});var r=e(3396);const u={class:"about"},t=(0,r._)("h1",null,"This is an about page",-1),c=[t];function s(n,a){return(0,r.wg)(),(0,r.iD)("div",u,c)}var i=e(89);const p={},o=(0,i.Z)(p,[["render",s]]);var b=o}}]);
+//# sourceMappingURL=about.c42ef939.js.map
