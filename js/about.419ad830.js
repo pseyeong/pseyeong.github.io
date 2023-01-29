4dce449ae32196e3d1b6765e3680b972acecdaf2
seyeong <yeongpsy@gmail.com>
2023-01-29 17:42:47 +0900

    230129

---
+++ js/about.419ad830.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkcomprj01"]=self["webpackChunkcomprj01"]||[]).push([[443],{7381:function(n,r,u){u.r(r),u.d(r,{default:function(){return l}});var t=u(3396);const a={class:"about"},c=(0,t._)("h1",null,"This is an about page",-1),e=[c];function s(n,r){return(0,t.wg)(),(0,t.iD)("div",a,e)}var o=u(89);const i={},f=(0,o.Z)(i,[["render",s]]);var l=f}}]);
+//# sourceMappingURL=about.419ad830.js.map
