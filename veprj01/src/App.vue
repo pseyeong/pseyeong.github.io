f741551e263d0e6479dd221485269fb24d25d0bf
se yeong <yeong.psy@gmail.com>
2023-02-06 18:05:38 +0900

    프로젝트 제작

--- veprj01/src/App.vue
+++ veprj01/src/App.vue
@@ -2,7 +2,7 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
-    <router-link to="/todo">Todo</router-link>
+    <!-- <router-link to="/todo">Todo</router-link> -->
   </nav>
   <router-view />
 </template>
