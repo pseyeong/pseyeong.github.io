4e8780e8a2ca9bcf513277f11ee38dc85eef01e2
pseyeong <yeong.psy@gmail.com>
2023-02-08 23:17:39 +0900

    json설정 및 텍스트 추가

--- veprj01/.eslintrc.js
+++ veprj01/.eslintrc.js
@@ -1,47 +1,32 @@
 module.exports = {
+  /*
+    https://feynubrick.github.io/2019/05/20/eslint-prettier.html
+    ESLint 규칙:
+        https://eslint.org/docs/rules/
+        https://eslint.org/docs/user-guide/configuring/
+    Prettier 옵션:
+        https://prettier.io/docs/en/options.html
+    */
   root: true,
   env: {
+    browser: false,
     node: true,
-    browser: true,
-    commonjs: true,
-    es6: true,
-    es2021: true,
-    jest: true,
-  },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
-  parserOptions: {
-    parser: "@babel/eslint-parser",
+    mocha: true,
   },
   plugins: ["prettier"],
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  parserOptions: {
+    parser: "babel-eslint",
+  },
+  ignorePatterns: ["node_modules/"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-
-    "prettier/prettier": ["error", { endOfLine: "auto" }],
-    "linebreak-style": "off",
-    "arrow-parens": ["error", "always"],
-    "arrow-body-style": "off",
-    "consistent-return": "off",
-    "func-names": "off",
-    "no-shadow": "off",
-    "no-alert": "off",
-    "no-var": "error",
-    "no-unused-vars": "off",
-    "no-param-reassign": ["error", { props: false }],
-  },
-  overrides: [
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
-      ],
-      env: {
-        jest: true,
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
       },
-    },
-  ],
+    ],
+  },
 };
