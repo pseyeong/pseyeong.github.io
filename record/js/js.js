20c63eb6186281d4e47efe52eb0d2e71373adb91
pseyeong <yeong.psy@gmail.com>
2023-02-14 23:57:58 +0900

    스크립트 1

---
+++ record/js/js.js
@@ -0,0 +1,10 @@
+var word = document.getElementById('word');
+var word_content = document.getElementById('word_content');
+var plus = document.querySelector('.plus');
+
+word.addEventListener('keypress',()=>{
+    console.log('test');
+
+    
+});
+
