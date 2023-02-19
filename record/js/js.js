3acbea0d672efebe76112955032476b36ffc677b
pseyeong <117248815+pseyeong@users.noreply.github.com>
2023-02-19 09:27:38 +0900

    Update js.js

--- record/js/js.js
+++ record/js/js.js
@@ -3,8 +3,6 @@ var word_content = document.getElementById('word_content');
 var plus = document.querySelector('.plus');
 
 word.addEventListener('keypress',()=>{
-    console.log('test');
-
-    
+   console.log(word);
 });
 
