4104f30d033b18e97fbb6b1f043af9f99faedbdd
pseyeong <yeong.psy@gmail.com>
2023-02-21 00:00:47 +0900

    23.02.20 스크립트 수정

--- record/js/js.js
+++ record/js/js.js
@@ -2,7 +2,21 @@ var word = document.getElementById('word');
 var word_content = document.getElementById('word_content');
 var plus = document.querySelector('.plus');
 
-word.addEventListener('keypress',()=>{
-   console.log(word);
+plus.addEventListener('click',(e)=>{
+    //console.log(word.value, word_content.value);
+    
+    let fr_memo = document.querySelector('.fr_memo');
+    let se_memo = document.querySelector('.se_memo');
+    let value = word.value;
+    let value_le = word_content.value;
+    //console.log(value);
+    fr_memo.innerHTML = value;
+    se_memo.innerHTML = value_le;
+
+    word.value = "";
+    word_content.value = "";
 });
+
+const modify = document.querySelector(".modify");
+
 
