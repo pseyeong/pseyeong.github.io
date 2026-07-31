9a74fca3966c27fe9d6a2d05892c6392f5834adb
pseyeong <yeong.psy@gmail.com>
2026-07-31 16:40:35 +0900

    카카오 지도 연습

--- src/main/java/com/pseyeong/blog/HomeController.java
+++ src/main/java/com/pseyeong/blog/HomeController.java
@@ -1,13 +1,15 @@
 package com.pseyeong.blog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("pageTitle", "지도");
         return "index";
     }
 }
